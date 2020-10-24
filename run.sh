3962c23672d7a1afdb669acae877426207b1ee33
hauh <hauh@ya.ru>
2020-10-24 07:51:57 +0300

    Fix run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PYTHON="python3.9"
+APP_NAME="quicksell_app"
 PORT="7777"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -13,11 +14,11 @@ source venv/bin/activate
 $PYTHON -m pip install -r requirements.txt 1>/dev/null
 
 echo "Making migrations..."
-$PYTHON quicksell/manage.py makemigrations 1>/dev/null
-$PYTHON quicksell/manage.py migrate 1>/dev/null
+$PYTHON quicksell/manage.py makemigrations $APP_NAME 1>/dev/null
+$PYTHON quicksell/manage.py migrate $APP_NAME 1>/dev/null
 
 echo "Starting server..."
-$PYTHON quicksell/manage.py runserver 0.0.0.0:$PORT 1>/dev/null &
+$PYTHON quicksell/manage.py runserver 0.0.0.0:$PORT 1>/dev/null & disown
 PID=$(echo $!)
 
 if [ ! -d /proc/$PID ]; then
