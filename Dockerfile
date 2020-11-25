5d02b49ad1f2bc512bfd2c91e2c52d43c223f19c
hauh <hauh@ya.ru>
2020-11-25 21:29:42 +0300

    Migrate to PostgreSQL

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM python:slim
-ENV PYTHONBUFFERED=1
-RUN apt-get update && apt-get upgrade -y
+ENV \
+	PYTHONBUFFERED=1 \
+	PYTHONDONTWRITEBYTECODE=1
+RUN \
+	apt-get update && apt-get upgrade -y && \
+	apt-get install -y libpq-dev gcc && \
+	apt-get clean
 WORKDIR /opt/app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- quicksell/quicksell/settings.py
+++ quicksell/quicksell/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
-from . import secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets.SECRET_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = secrets.ALLOWED_HOSTS
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
 AUTH_USER_MODEL = 'quicksell_app.User'
 
@@ -84,11 +84,15 @@ WSGI_APPLICATION = 'quicksell.wsgi.application'
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
-	}
-}
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': os.environ['POSTGRES_DB'],
+		'USER': os.environ['POSTGRES_USER'],
+		'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+		'HOST': 'quicksell_db',
+		'PORT': 5432,
+	},
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
