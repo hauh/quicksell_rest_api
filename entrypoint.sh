f1231ef51b20436f09b82f20246955e3fc8aa8cf
hauh <hauh@ya.ru>
2021-03-29 22:23:42 +0300

    Save migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ postfix start
 if [ ! -f .initialized ]; then
 	set -e
 	echo "Migrating database..."
-	python manage.py makemigrations
-	python manage.py migrate
+	python manage.py makemigrations quicksell_app
 	python manage.py migrate quicksell_app
 	echo "Collecting static files..."
 	python manage.py collectstatic --no-input
